b86438cc1518521b1718b8043cf01f361720018f
xean <xeanloo@gmail.com>
2025-11-04 10:46:58 +0100

    Versuch2: 2d on progress

--- Versuch2/houghCircle_demo.m
+++ Versuch2/houghCircle_demo.m
@@ -1,14 +1,23 @@
 I = imread('bilder/Zellen.jpg');
 grayI = rgb2gray(I);
 grayI = imgaussfilt(grayI, 5);
-edges = edge(grayI, 'Canny');
-[mOut, nOut, rOut] = houghCircle(edges, 50, 10, 20);
+% grayI = imbilatfilt(grayI, 75, 9);
+edges = edge(grayI, 'canny');
+[mOut, nOut, rOut] = houghCircle(edges, 100, 10, 35);
 
-figure(1); subplot(1,2,1);
+disp(size(rOut, 2) + " circles detected.");
+
+figure(1); subplot(1,3,1);
 imshow(I);
 plotCircle(mOut, nOut, rOut);
 title('Circles detected');
 
-figure(1); subplot(1,2,2);
+figure(1); subplot(1,3,2);
 imshow(edges);
 title('Edge Image');
+
+figure(1); subplot(1,3,3);
+histogram(rOut);
+title('Radius Histogram');
+xlabel('Radius (pixels)');
+ylabel('Frequency');
