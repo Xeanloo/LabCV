0b45a70da92c1040dceff83af289d314d605136f
xean <xeanloo@gmail.com>
2025-11-10 19:39:17 +0100

    Versuch 3 init

---
+++ Versuch3/regionGrowing.m
@@ -0,0 +1,10 @@
+function S = regionGrowing(I, xStart, yStart, threshold)
+% Eingabe: I              - Bild
+%          xStart, yStart - Saatpunkt
+%          threshold      - Schwellwert für die Ähnlichkeit in der Region
+%
+% Einfacher Steppenbrand-Algorithmus.
+
+    % TODO
+
+end
