d46d91b6a4adb85f9b2e83b17debabda2282afac
Xean Effendy <xean.effendy@meko.de>
2025-11-25 11:36:34 +0100

    add meanshift plots

--- Versuch5/meanshift_demo.m
+++ Versuch5/meanshift_demo.m
@@ -4,11 +4,15 @@ clear; clc; close all;
 dataFile = 'test_data_clustering.mat';
 dataStruct = load(dataFile);
 varNames = fieldnames(dataStruct);
+bArr = [1.0, 0.75, 0.5,1.5];
+
 
 for idx = 1:numel(varNames)-1
     pts = dataStruct.(varNames{idx});
-    b = 2;
+    b = bArr(idx);
     [L, C] = meanshift(pts, b, 0);
     disp(unique(L));
     disp(C);
+    figure;
+    plotClusterResults(pts, L);
 end
