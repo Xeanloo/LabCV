ad126eca1c2bc1027a672d491993fdab79063b29
Sohan <me.sohandeshar+ghhp460@gmail.com>
2026-01-13 12:09:46 +0100

    fixed mse

--- Versuch9/kalibrierung.m
+++ Versuch9/kalibrierung.m
@@ -73,15 +73,16 @@ imshow(img1); hold on;
 % draw the projected points
 plot(projPoints1(:,1), projPoints1(:,2), 'go', 'MarkerSize', 10);
 plot(points1(:,1), points1(:,2), 'r*', 'MarkerSize', 10);
-error1 = mean(sqrt(sum((points1(:,1:2) - projPoints1).^2, 2)));
-title(sprintf('MSE: %.2f', error1));
+error1 = sqrt(sum((points1(:,1:2) - projPoints1).^2, 2));
+meanError1 = mean(error1(points1(:,1)>0));
+title(sprintf('MSE: %.2f', meanError1));
 figure(2); clf;
 imshow(img2); hold on;
 plot(projPoints2(:,1), projPoints2(:,2), 'go', 'MarkerSize', 10);
 plot(points2(:,1), points2(:,2), 'r*', 'MarkerSize', 10);
-error2 = mean(sqrt(sum((points2(:,1:2) - projPoints2).^2, 2)));
-title(sprintf('MSE: %.2f', error2));
-hold off;
+error2 = sqrt(sum((points2(:,1:2) - projPoints2).^2, 2));
+meanError2 = mean(error2(points2(:,1)>0));
+title(sprintf('MSE: %.2f', meanError2));
 
 %% Aufgabe 2: Szenenrekonstruktion und Triangulation
 % a) Bestimmung der Projektionsmatrix der zweiten Kamera
