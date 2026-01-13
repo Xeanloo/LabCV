ac3d92b8f529b280e25de8958cd761079d6aaf7b
Xean Effendy <xean.effendy@meko.de>
2026-01-13 09:09:30 +0100

    add points and plots for part1

--- Versuch9/kalibrierung.m
+++ Versuch9/kalibrierung.m
@@ -23,15 +23,26 @@ view(-30, 30); axis off;
 % Projizierte Eckpunkte des Würfels bestimmen (getPoints.m)
 
 % TODO
-points1 = [3.050839416058394e+02, 2.383978102189781e+02; ...
-           3.485291970802919e+02, 2.278868613138686e+02; ...
-           2.546313868613138e+02, 3.603248175182482e+02; ...
-           3.043832116788320e+02, 3.365000000000000e+02; ...
-           1.404124087591240e+02, 2.993613138686131e+02; ...
+points1 = [255.8321, 362.5493; ...
+           303.1945, 337.9573; ...
+           305.9269, 237.7676; ...
+           346.9137, 228.6594; ...
+           141.9801, 301.5247; ...
            0, 0; ...
-           1.873613138686131e+02, 1.795364963503649e+02; ...
-           2.476240875912408e+02, 1.753321167883212e+02];
-points1 = getPoints(img1);
+           188.4317, 180.3861; ...
+           242.1698, 174.9213];
+
+points2 = [
+      223.9535,  369.8359; ...
+      340.5380,  347.0655; ...
+      233.0617,  237.7676; ...
+      354.2002,  221.3729; ...
+      170.2154,  311.5436; ...
+             0,         0; ...
+      173.8586,  192.2268; ...
+      288.6214,  179.4753; ...
+]
+% points1 = getPoints(img2);
 
 % points2 = getPoints(img2);
 
@@ -42,10 +53,10 @@ points1 = getPoints(img1);
 % TODO
 
 valid1 = points1(:,1) > 0;
-% valid2 = points2(:,1) > 0;
+valid2 = points2(:,1) > 0;
 
 projectionMatrix1 = getProjectionMatrix(Xw(valid1, :), points1(valid1, :));
-% projectionMatrix2 = getProjectionMatrix(Xw(valid2, :), points2(valid2, :));
+projectionMatrix2 = getProjectionMatrix(Xw(valid2, :), points2(valid2, :));
 
 %% Aufgabe 1c) Test (Rückprojektion)
 % Test: Projektion der Eckpunkte (X_w) des Kalibrierungsobjekts in das Bild
@@ -55,25 +66,27 @@ projectionMatrix1 = getProjectionMatrix(Xw(valid1, :), points1(valid1, :));
 % TODO
 projPoints1_hom = (projectionMatrix1 * [Xw, ones(size(Xw, 1), 1)]')';
 projPoints1 = projPoints1_hom(:, 1:2) ./ projPoints1_hom(:, 3);
-% projPoints2_hom = (projectionMatrix2 * [Xw, ones(size(Xw, 1), 1)]')';
-% projPoints2 = projPoints2_hom(:, 1:2) ./ projPoints2_hom(:, 3);
+projPoints2_hom = (projectionMatrix2 * [Xw, ones(size(Xw, 1), 1)]')';
+projPoints2 = projPoints2_hom(:, 1:2) ./ projPoints2_hom(:, 3);
 
 figure(1); clf;
 imshow(img1); hold on;
 % draw the projected points
-plot(projPoints1(:,1), projPoints1(:,2), 'r*', 'MarkerSize', 10);
+plot(projPoints1(:,1), projPoints1(:,2), 'go', 'MarkerSize', 10);
+plot(points1(:,1), points1(:,2), 'r*', 'MarkerSize', 10);
 title('Projektion der Kalibrierungspunkte in Bild 1');
-% figure(2); clf;
-% imshow(img2); hold on;
-% plot(projPoints2(:,1), projPoints2(:,2), 'r*', 'MarkerSize', 10);
-% title('Projektion der Kalibrierungspunkte in Bild 2');
+figure(2); clf;
+imshow(img2); hold on;
+plot(projPoints2(:,1), projPoints2(:,2), 'go', 'MarkerSize', 10);
+plot(points2(:,1), points2(:,2), 'r*', 'MarkerSize', 10);
+title('Projektion der Kalibrierungspunkte in Bild 2');
 error1 = sqrt(sum((points1(:,1:2) - projPoints1).^2, 2));
 meanError1 = mean(error1(points1(:,1)>0));
-% error2 = sqrt(sum((points2(:,1:2) - projPoints2).^2, 2));
-% meanError2 = mean(error2(points2(:,1)>0));
+error2 = sqrt(sum((points2(:,1:2) - projPoints2).^2, 2));
+meanError2 = mean(error2(points2(:,1)>0));
 fprintf('Mittlerer Rückprojektionsfehler Bild 1: %.2f Pixel\n', meanError1);
 hold off;
-% fprintf('Mittlerer Rückprojektionsfehler Bild 2: %.2f Pixel\n', meanError2);       
+fprintf('Mittlerer Rückprojektionsfehler Bild 2: %.2f Pixel\n', meanError2);       
 %% Aufgabe 2: Szenenrekonstruktion und Triangulation
 % a) Bestimmung der Projektionsmatrix der zweiten Kamera
 
