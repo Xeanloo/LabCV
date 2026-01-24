0a0acde5fa10e3521354640511a54bb4820522dd
Sohan <me.sohandeshar+ghhp460@gmail.com>
2026-01-24 12:36:57 +0100

    Lab 11 init

---
+++ Versuch11/TDLS.m
@@ -0,0 +1,11 @@
+function p = TDLS(M, xStart, yStart, w)
+% Block Matching (Two Dimensional Logarithmic Search)
+%
+% Eingabe: M - Video, (m x n x 3 x t)-Array
+%          xStart, yStart - Startpunkt
+%          w - Fenstergröße
+%
+% Ausgabe: p - (2 x t)-Array, verfolgter Pfad.
+
+
+end
